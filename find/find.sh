29235eb212bd7d7ea47386b0e4636b9b7840cf10
namhoon.lee <namhoon.lee@streami.co>
2020-01-06 10:12:45 +0900

    update

--- find/find.sh
+++ find/find.sh
@@ -15,4 +15,5 @@ return 'found'
 # %T+: Date and time, separated by `+', for example `2004-04-28+22:22:05.0'.  This is a GNU  extension.
 # The  time  is given in the current timezone (which may be affected by setting the TZ 
 # environment variable).  The seconds field includes a fractional part.
-find . -printf "%T+\n"
+# -prune -o: 앞의 -path 에 포함된 directory 를 배제하고 찾기를 수행한다.
+find . -path ./.git -prune -o -printf "%T+%p\n"
